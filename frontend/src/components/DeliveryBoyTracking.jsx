97ce6ddb01130af471544acee471e0c9c61945cd
adarsh-priydarshi-5646 <adarshpriydarshi5646@gmail.com>
2025-10-01 15:24:38 +0530

    Fix: Add Leaflet marker icons for delivery tracking map

--- frontend/src/components/DeliveryBoyTracking.jsx
+++ frontend/src/components/DeliveryBoyTracking.jsx
@@ -10,15 +10,26 @@ import {
   Popup,
   TileLayer,
 } from "react-leaflet";
+
+// Fix Leaflet default marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 const deliveryBoyIcon = new L.Icon({
   iconUrl: scooter,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
+  popupAnchor: [0, -40],
 });
 const customerIcon = new L.Icon({
   iconUrl: home,
   iconSize: [40, 40],
   iconAnchor: [20, 40],
+  popupAnchor: [0, -40],
 });
 function DeliveryBoyTracking({ data }) {
   const deliveryBoyLat = data.deliveryBoyLocation.lat;
